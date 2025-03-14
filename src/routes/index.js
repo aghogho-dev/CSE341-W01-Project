784f7849ab6c4a75dc53971cfcad1afdd7c2ccee
Aghogho <monorienaghogho@gmail.com>
2025-03-14 12:55:35 +0100

    Update host to json

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.use("/", require("./swagger"));
 
-router.get("/", (req, res) => res.send("Hello World"));
+router.get("/", (req, res) => res.json({message: "Hello World"}));
 
 router.use("/contacts", require("./contacts"));
 
