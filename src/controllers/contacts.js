c36eab1996921bc21e0d48d9e7b16e1d179ecc70
Aghogho <monorienaghogho@gmail.com>
2025-03-14 11:20:26 +0100

    W02 - PUT POST DELETE routes

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -13,15 +13,72 @@ const getAll = async (req, res) => {
 
 const getOne = async (req, res) => {
     const userId = ObjectId.createFromHexString(req.params.id);
-    const result = await mongodb.getDatabase().db("W01_Project").collection("Contacts").find({ _id: userId });
-    result.toArray()
-        .then((contacts) => {
-            res.setHeader("Content-Type", "application/json");
-            res.status(200).json(contacts[0]);
-        });
+    const result = await mongodb.getDatabase().db("W01_Project").collection("Contacts").findOne({ _id: userId });
+
+    console.log(result);
+    if (result === null) {
+        return res.status(404).json({error: "No contact found"});
+    }
+   
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(result);
+   
 };
+
+
+const create = async (req, res) =>  {
+    const newContact = req.body;
+    const result = await mongodb.getDatabase().db("W01_Project").collection("Contacts").insertOne(newContact);
+
+    if (result.acknowledged) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).json(
+            {
+                _id: result.insertedId,
+                ...newContact
+            }
+        );
+    } else {
+        res.status(500).json(response.error || "Error creating contact");
+    }
+}
+
+const update = async (req, res) => {
+
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const updatedContact = req.body;
+
+    if (Object.keys(updatedContact).length === 0) {
+        return res.status(400).json({error: "No data to update"});
+    }
+
+    const result = await mongodb.getDatabase().db("W01_Project").collection("Contacts").updateOne({ _id: userId }, { $set: updatedContact });
+
+    if (result.matchedCount === 0) {
+        return res.status(400).json({error: "No contact matched the id you want to update"});
+    }
+
+    res.status(200).json({message: "Contact updated successfully"});
+}
+
+const remove = async (req, res) => {
+
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const result = await mongodb.getDatabase().db("W01_Project").collection("Contacts").deleteOne({ _id: userId});
+
+    if (result.deletedCount === 0) {
+        return res.status(400).json({error: "No contact matched the id you want to delete"});
+    }
+
+    res.status(200).json({message: "Contact deleted successfully"});
+}
+
+
 
 module.exports = {
     getAll,
     getOne,
+    create,
+    update,
+    remove
 }
