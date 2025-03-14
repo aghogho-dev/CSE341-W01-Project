292e89dcbf50d8ce665cef685c8713a6befaffbe
Aghogho <monorienaghogho@gmail.com>
2025-03-14 13:04:33 +0100

    Update cors

--- src/server.js
+++ src/server.js
@@ -4,17 +4,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     next();
+// });
 
 app.use("/", routes);
 
